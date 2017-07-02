74f1d9589c73cc3559345375b1aee753afa2090c
Jorge <ygd182@gmail.com>
2017-07-02 13:32:28 -0300

    added socioModel

---
+++ models/SocioModel.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var tiposSocioArray = ['vitalicio', 'mayor', 'menor'];
+
+var Socio = new Schema ({ 
+	nroSocio: {
+        type: Number,
+        required: true
+    },
+    nombre: String,
+    dni: String,
+    direccion: {
+        calle: String,
+        numero: String,
+        ciudad: String,
+        codigoPostal: Number
+    },
+    tipoSocio: { type: String, enum: tiposSocioArray}
+
+});
+
+module.exports = mongoose.model('Socio', SocioSchema);
